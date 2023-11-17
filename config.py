7973a29582b748c54b067820149fe9919d0ed5a7
jeon-yunsu <137169311+jeon-yunsu@users.noreply.github.com>
2023-11-17 16:03:11 +0900

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-config = {
-    'user': 'movie',
-    'password': 'movie123!',
-    'host': 'moblemoviedb.cqsuhzthybj4.ap-northeast-2.rds.amazonaws.com',
-    'database': 'movie',
-    'raise_on_warnings': True
-}
