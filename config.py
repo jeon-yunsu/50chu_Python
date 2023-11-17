6a6fe98bf5ffff67b3328aa29f9f9e4b1973e3fd
전윤수 <jeon-yunsu@jeon-yunsuui-MacBookPro.local>
2023-11-17 16:02:09 +0900

    algorithm

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+    'user': 'movie',
+    'password': 'movie123!',
+    'host': 'moblemoviedb.cqsuhzthybj4.ap-northeast-2.rds.amazonaws.com',
+    'database': 'movie',
+    'raise_on_warnings': True
+}
